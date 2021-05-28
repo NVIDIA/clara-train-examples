f5fee767b8bcec6a10da2621c625000c1312a562
Ahmed Harouni <aharouni@nvidia.com>
2021-05-28 16:48:11 -0700

    Adding AIAAwOHIF

---
+++ PyTorch/NoteBooks/AIAA/OHIF-Orthanc/restart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: Apache-2.0
+
+chmod 664 nginx/config/.htpasswd
+./stop.sh
+sleep 2
+echo -----------------------------
+docker rmi aiaa-ohif:v4.0
+#docker-compose build --no-cache
+sleep 2
+echo -----------------------------
+docker-compose -p aiaa-orthanc up --remove-orphans -d
+
+
+for i in {1..6}
+do
+   echo ---wait $i
+   sleep 1
+done
+docker logs claradevday-pt 2>&1 |grep token= | head -2
