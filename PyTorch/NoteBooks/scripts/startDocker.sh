ef62b8caa20b36ff293e2257c8cd4b2c9bf8fed9
Ahmed Harouni <aharouni@nvidia.com>
2022-05-05 14:39:56 -0700

    C

--- PyTorch/NoteBooks/scripts/startDocker.sh
+++ PyTorch/NoteBooks/scripts/startDocker.sh
@@ -2,25 +2,23 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-#DOCKER_IMAGE=clara-train-nvdashboard:v4.0
-DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v4.0
+DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v4.1
+#DOCKER_IMAGE=clara-train-nvdashboard:4.1
+
 
 DOCKER_Run_Name=claradevday-pt
 
 jnotebookPort=$1
 GPU_IDs=$2
-AIAA_PORT=$3
+#AIAA_PORT=$3
 #################################### check if parameters are empty
 if [[ -z  $jnotebookPort ]]; then
     jnotebookPort=3030
 fi
 if [[ -z  $GPU_IDs ]]; then  #if no gpu is passed
-    # for all gpus use line below
-    GPU_IDs=all
-    # for 2 gpus use line below
-    #GPU_IDs=2
-    # for specific gpus as gpu#0 and gpu#2 use line below
-#    GPU_IDs='"device=1,2,3"'
+    GPU_IDs=all  # for all gpus
+    #GPU_IDs=2 # for 2 gpus use line below
+#    GPU_IDs='"device=1,2,3"'   # for specific gpus as gpu#0 and gpu#2 use line below
 fi
 #################################### check if name is used then attache to it
 docker ps -a|grep ${DOCKER_Run_Name}
@@ -31,11 +29,9 @@ if ((${dockerNameExist}==0)) ;then
   docker exec -it ${DOCKER_Run_Name} /bin/bash
   exit
 fi
-
 echo -----------------------------------
 echo starting docker for ${DOCKER_IMAGE} using GPUS ${GPU_IDs} jnotebookPort ${jnotebookPort}
 echo -----------------------------------
-
 if true; then # immediately start Jupyter
   extraFlag="-d "
   cmd2run="jupyter lab /claraDevDay --ip 0.0.0.0 --port 8888 --allow-root --no-browser"
@@ -44,27 +40,29 @@ else # run interactively
   cmd2run="/bin/bash"
 fi
 
-extraFlag=${extraFlag}" -p "${jnotebookPort}":8888 -p "3031":5000"
+extraFlag=${extraFlag}" -p "${jnotebookPort}":8888 -p 3031:5000"
 #extraFlag=${extraFlag}" --net=host "
 #extraFlag=${extraFlag}" -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group "
 
-  #--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+#For the clara viz to work
+extraFlag=${extraFlag}" -e NVIDIA_DRIVER_CAPABILITIES=graphics,video,compute,utility"
+#--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
 docker run --rm ${extraFlag} \
   --name=${DOCKER_Run_Name} \
   --gpus ${GPU_IDs} \
   -v ${PWD}/../:/claraDevDay/ \
-  -v /raid/users/aharouni/data:/data \
-  -w /claraDevDay/scripts \
+  -v ${PWD}/../../MONAI:/monai/ \
+  -v /raid/users/aharouni/data/:/data/ \
+  -w /claraDevDay \
   --runtime=nvidia \
   --ipc=host \
   ${DOCKER_IMAGE} \
   ${cmd2run}
 
-for i in {1..6}
+for i in {1..4}
 do
    echo ---wait $i
    sleep 1
 done
-docker logs claradevday-pt 2>&1 |grep token= | head -2
 
-#echo ------------------ exited from docker image
+docker logs ${DOCKER_Run_Name} 2>&1 |grep token= | tail -2
