debc51b47b6e657e8255c36d93aee2189e1ef319
Ahmed Harouni <aharouni@nvidia.com>
2022-05-05 14:48:39 -0700

    Updating to v4.1

--- PyTorch/NoteBooks/scripts/stopClaraTrainNoteBooks.sh
+++ PyTorch/NoteBooks/scripts/stopClaraTrainNoteBooks.sh
@@ -2,4 +2,10 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
+# Stop container if started with StartDocker.sh
+echo Stopping claradevday-pt
+docker stop claradevday-pt
+
+echo Stopping docker compose
 docker-compose -p claratrain-triton down --remove-orphans
+
